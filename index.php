337a8a5d30434f6caec01cec789fc07110ca403f
kaiqtadeu <95945122+kaiqtadeu@users.noreply.github.com>
2022-12-01 02:20:21 -0300

    Login
    Alinhamento do botão entrar.

--- index.php
+++ index.php
@@ -1,53 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+      <title>Login</title>
+    </head>
+  <body>
 
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Cadastro</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="?page=home">Home</a>
-        </li>
-        <li>
-           <a class="button btn btn-primary nav-link text-white" href="index.php?page=login">Entrar</a>
-        </li>
-      </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light m-4">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Cadastro</a>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarNav">
+
+          <ul class="navbar-nav">
+
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="?page=home">Home</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="?page=home">Quem somos</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="?page=home">a ideia</a>
+            </li>
+
+          </ul>
+            <a class="button btn btn-primary text-white ms-auto" href="index.php?page=login">Entrar</a>
+        </div>     
+      </div>
+    </nav>
+
+
+
+    <div class="container m-4">
+      <div class="row">
+        <div class="col mt-5">
+            <?php
+                include('conexao.php');
+                if(isset($_REQUEST['page']) && file_exists($_REQUEST['page'].'.php')){
+                  include_once($_REQUEST['page'].'.php');
+                }else{
+                  echo "<h1> Olá carai </h1>";
+                }
+            ?>
+        </div>
+      </div>
     </div>
-  </div>
-</nav>
-
-<div class="container">
-  <div class="row">
-    <div class="col mt-5">
-        <?php
-            include('conexao.php');
-
-            if(isset($_REQUEST['page']) && file_exists($_REQUEST['page'].'.php')){
-              include_once($_REQUEST['page'].'.php');
-            }else{
-
-              echo "<h1> Olá carai </h1>";
-             
-            }
-        ?>
-    </div>
-  </div>
-</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
 
-</body>
+  </body>
 </html>
